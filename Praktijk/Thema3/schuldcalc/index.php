cfc2e61deff4429845a44e9e6761461782be45a4
zkainee <kainevommeren@gmail.com>
2022-02-21 09:46:45 +0100

    T3_PRA_SchuldCalculator_0_1

---
+++ Praktijk/Thema3/schuldcalc/index.php
@@ -0,0 +1,34 @@
+<?php
+/* User: Kaine van Ommeren
+ * Date: 17-02-2022
+ * File: index.php
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            <?php
+                echo 'Hofman Incasso';
+            ?>
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Hofman Incasso">
+        <link rel="stylesheet" href="styles/base_style.css">
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <nav>
+
+        </nav>
+        <main>
+
+        </main>
+        <section>
+
+        </section>
+    </body>
+</html>
