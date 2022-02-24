2419b17c067bfb560bf891a1c98e33564aafe9ac
zkainee <kainevommeren@gmail.com>
2022-02-24 19:47:49 +0100

    Minor changes

--- Realiseren/Thema3/Hoofdstuk2/Oefening/2.2/T3_REA_Oefening2.2.php
+++ Realiseren/Thema3/Hoofdstuk2/Oefening/2.2/T3_REA_Oefening2.2.php
@@ -33,9 +33,19 @@
             <h2>Thema 3</h2>
             <ul>
                 <li>Hoofdstuk 1:</li>
+                <li>
+                    Hoofdstuk 2:
+                </li>
                 <ul>
                     <li><a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/2.1/T3_REA_Oefening2.1.php">Oefening 2.1</a></li>
                     <li><a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/2.2/T3_REA_Oefening2.2.php">Oefening 2.2</a></li>
+                </ul>
+                <li>
+                    Hoofdstuk 3:
+                </li>
+                <ul>
+                    <li><a href="/Realiseren/Thema3/Hoofdstuk3/Oefening/3.1/T3_REA_Oefening3_1.php">Oefening 3.1</a> </li>
+                    <li><a href="/Realiseren/Thema3/Hoofdstuk3/Oefening/3.2/T3_REA_Oefening3_2.php">Oefening 3.2</a> </li>
                 </ul>
             </ul>
         </nav>
