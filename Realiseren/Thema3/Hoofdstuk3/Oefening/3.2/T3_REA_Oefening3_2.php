7433b173b7ba5355f875979e0a30ee74028f66d9
zkainee <kainevommeren@gmail.com>
2022-02-24 19:46:25 +0100

    Minor changes

---
+++ Realiseren/Thema3/Hoofdstuk3/Oefening/3.2/T3_REA_Oefening3_2.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * User: Kaine van Ommeren
+ * Date: 24-02-2022
+ * File: T3_REA_Oefening3.2.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+            <?php
+                echo 'Realiseren instructies thema 3 en 4.';
+            ?>
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="styles/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>
+                <?php
+                    echo 'Uitwerkingen van PHP-oefeningen';
+                ?>
+            </h1>
+        </header>
+        <?php
+            //Hier gaan we in het volgende hoofdstuk het menu toevoegen.
+        ?>
+        <main id="wrapper">
+            <nav>
+                <h2>Thema 3</h2>
+                <ul>
+                <li>Hoofdstuk 1:</li>
+                <li>
+                    Hoofdstuk 2:
+                </li>
+                    <ul>
+                        <li><a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/2.1/T3_REA_Oefening2.1.php">Oefening 2.1</a></li>
+                        <li><a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/2.2/T3_REA_Oefening2.2.php">Oefening 2.2</a></li>
+                    </ul>
+                <li>
+                    Hoofdstuk 3:
+                </li>
+                    <ul>
+                        <li><a href="/Realiseren/Thema3/Hoofdstuk3/Oefening/3.1/T3_REA_Oefening3_1.php">Oefening 3.1</a> </li>
+                        <li><a href="/Realiseren/Thema3/Hoofdstuk3/Oefening/3.2/T3_REA_Oefening3_2.php">Oefening 3.2</a> </li>
+                    </ul>
+                </ul>
+            </nav>
+            <section>
+                <h2>
+                    Uitwerking
+                </h2>
+
+                <!-- Plaats je code / uitwerking hieronder -->
+                <p>
+                    <?php
+                        // Voorbeeldcode
+                        echo "Op deze plek komt de uitwerking van de oefening te staan.";
+                    ?>
+                </p>
+            </section>
+        </main>
+    </body>
+</html>
+
