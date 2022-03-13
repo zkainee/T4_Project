2c563d27477f0fd1e459d35d25dcc4ce91d8215d
zkainee <kainevommeren@gmail.com>
2022-03-13 12:26:34 +0100

    T3_REA_Oefening3_2.php

---
+++ Realiseren/Thema3/includes/footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: Kaine van Ommeren
+ * Date: 13-03-2022
+ * File: footer.php
+ */
+?>
+<?php
+    $date = date("d-m-Y");
+    $footer =
+    "
+    <footer>
+    Kaine van Ommeren | IOS1SAV | $date
+    </footer>";
