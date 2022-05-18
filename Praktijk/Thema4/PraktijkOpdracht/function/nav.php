d92d42a3e604685ee6e37aa6194c5afe57b54cbe
zkainee <kainevommeren@gmail.com>
2022-05-18 11:08:40 +0200

    PraktijkOpdracht website patch 0.1

---
+++ Praktijk/Thema4/PraktijkOpdracht/function/nav.php
@@ -0,0 +1,7 @@
+<?php
+?>
+<nav>
+    <ul class="menuNav">
+        <li><a href="../pages/login.php">Login</a></li>
+    </ul>
+</nav>
--- Praktijk/Thema4/PraktijkOpdracht/index.php
+++ Praktijk/Thema4/PraktijkOpdracht/index.php
@@ -8,20 +8,14 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+        <link rel="stylesheet" href="./styles/index.css">
+        <title>Kuijpers</title>
     </head>
     <body>
-        <form method="post" action="login.php">
-            <p>
-                Username:
-            </p>
-            <input type="text" name="txtUsername" required>
-            <p>
-                Password:
-            </p>
-            <input type="password" name="txtPassword" required>
-            <input type="submit" value="Login">
-        </form>
+        <?php
+        include "./function/nav.php"
+        ?>
+        <img src="./pictures/kuijpers.png" id="kuijpersBackground">
     </body>
 </html>
 
